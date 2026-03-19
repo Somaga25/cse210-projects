50ba7614ce9fbca67d38983ef579819e2c6a8689
Somaga25 <ceke2@byupathway.edu>
2026-03-19 22:48:13 +0100

    addjob method added

--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -4,17 +4,24 @@ using System.Collections.Generic;
 public class Resume
 {
     public string _name;
-
     public List<Job> _jobs = new List<Job>();
+
+    public void AddJob(Job job)
+    {
+        _jobs.Add(job);
+    }
 
     public void Display()
     {
+        Console.WriteLine("===== RESUME =====");
         Console.WriteLine($"Name: {_name}");
-        Console.WriteLine("Jobs:");
+        Console.WriteLine();
 
         foreach (Job job in _jobs)
         {
             job.Display();
         }
+
+        Console.WriteLine("==================");
     }
 }
